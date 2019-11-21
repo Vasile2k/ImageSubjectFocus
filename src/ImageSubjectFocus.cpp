f7bc1f00fdd991f39b2bf7e5800c61a1e50b7ab8
Vasile2kappa <andy.dorcu@yahoo.com>
2019-11-21 12:53:01 +0200

    Fixed tab indent.

--- src/ImageSubjectFocus.cpp
+++ src/ImageSubjectFocus.cpp
@@ -4,6 +4,6 @@
 
 int main(){
 	stbi_set_flip_vertically_on_load(1); // Just to test if it builds
-    std::cout << "The horse is in the house" << log10(100) << std::endl;
+	std::cout << "The horse is in the house" << log10(100) << std::endl;
 	return 0;
 }
