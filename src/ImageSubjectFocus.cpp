dcea3ae8efe86953a3e5c3abe21518ca20a2cc89
Vasile2kappa <andy.dorcu@yahoo.com>
2019-11-21 12:50:56 +0200

    Fixed newline

--- src/ImageSubjectFocus.cpp
+++ src/ImageSubjectFocus.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include "pch.h"#include "stb_image.h"
+#include "pch.h"
+#include "stb_image.h"
 
 int main(){
 	stbi_set_flip_vertically_on_load(1); // Just to test if it builds
