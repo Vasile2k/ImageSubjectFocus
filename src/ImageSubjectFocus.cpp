bb12a414553251c95be89c51b0d216a13005026e
Vasile2kappa <andy.dorcu@yahoo.com>
2019-12-13 16:42:30 +0200

    Add edge strength and some fixes.

--- src/ImageSubjectFocus.cpp
+++ src/ImageSubjectFocus.cpp
@@ -18,9 +18,16 @@ union doubleColor {
 	};
 };
 
-doubleColor rgbToCielab(color in);
-color guessBackgroundColor(uint8_t* image, int w, int h);
 color averageBlockColor(uint8_t* image, int w, int h, int xStart, int xStop, int yStart, int yStop);
+color guessBackgroundColor(uint8_t* image, int w, int h);
+doubleColor colorToDoubleColor(color in);
+color doubleColorToColor(doubleColor in);
+double linearizeColorComponent(double color);
+doubleColor linearizeRgb(doubleColor in);
+doubleColor rgbToCiexyz(doubleColor in);
+doubleColor ciexyzToCielab(doubleColor in);
+doubleColor rgbToCielab(color in);
+double colorToGrayscale(doubleColor in);
 
 std::vector<double*> maps;
 
@@ -41,8 +48,8 @@ int main(){
 	/////////////////////////////////
 
 	map = new double[mapWidth * mapHeight];
-	for (int i = 0; i < x - 8; i += 4) {
-		for (int j = 0; j < y - 8; j += 4) {
+	for (int i = 0; i <= x - 8; i += 4) {
+		for (int j = 0; j <= y - 8; j += 4) {
 			color blockColor = averageBlockColor(image, x, y, i, i+8, j, j+8);
 			doubleColor blockCieColor = rgbToCielab(blockColor);
 
@@ -58,8 +65,8 @@ int main(){
 
 	map = new double[mapWidth * mapHeight];
 
-	for (int i = 0; i < x - 8; i += 4) {
-		for (int j = 0; j < y - 8; j += 4) {
+	for (int i = 0; i <= x - 8; i += 4) {
+		for (int j = 0; j <= y - 8; j += 4) {
 			color blockColor = averageBlockColor(image, x, y, i, i+8, j, j+8);
 			doubleColor blockCieColor = rgbToCielab(blockColor);
 
@@ -77,8 +84,8 @@ int main(){
 
 	map = new double[mapWidth * mapHeight];
 
-	for (int i = 0; i < x - 8; i += 4) {
-		for (int j = 0; j < y - 8; j += 4) {
+	for (int i = 0; i <= x - 8; i += 4) {
+		for (int j = 0; j <= y - 8; j += 4) {
 
 			double l[64];
 			int lIndex = 0;
@@ -86,11 +93,11 @@ int main(){
 				for (int yy = 0; yy < 8; ++yy) {
 
 					int pixelIndex = (j+yy)*x + (i+xx);
-					double rr = (double)image[3*pixelIndex] / UINT8_MAX;
-					double gg = (double)image[3*pixelIndex + 1] / UINT8_MAX;
-					double bb = (double)image[3*pixelIndex + 2] / UINT8_MAX;
+					uint8_t rr = image[3*pixelIndex];
+					uint8_t gg = image[3*pixelIndex + 1];
+					uint8_t bb = image[3*pixelIndex + 2];
 
-					double grayscaleRms = rr * 0.299 + gg * 0.587 + bb * 0.114;
+					double grayscaleRms = colorToGrayscale(colorToDoubleColor({rr, gg, bb}));
 
 					const double _b = 0.7297;
 					const double _k = 0.037644;
@@ -118,6 +125,71 @@ int main(){
 			map[j/4 * mapWidth + i/4] = (mean > 0) ? dispersion/mean : 0;
 		}
 	}
+	maps.push_back(map);
+
+	/////////////////////////////////
+	// MAP 4 - sharpness
+	/////////////////////////////////
+
+	map = new double[mapWidth * mapHeight];
+
+	for (int i = 0; i <= x - 32; i += 8) {
+		for (int j = 0; j <= y - 32; j += 8) {
+
+		}
+	}
+
+	maps.push_back(map);
+
+	/////////////////////////////////
+	// MAP 5 - edge strength
+	/////////////////////////////////
+
+	map = new double[mapWidth * mapHeight];
+
+	for (int i = 0; i <= x - 8; i += 4) {
+		for (int j = 0; j <= y - 8; j += 4) {
+			// Roberts edge detector
+			// Since kernel is 2x2 we're gonna miss one pixel from one edge
+			// Hence, the size of the resulting block is 7x7
+			double robertsValues[7*7];
+			int robertsValuesIndex = 0;
+
+			for (int xx = 0; xx < 7; ++xx) {
+				for (int yy = 0; yy < 7; ++yy) {
+
+					int pixelIndex0 = (j+yy)*x + (i+xx);
+					int pixelIndex1 = (j+yy)*x + (i+xx+1);
+					int pixelIndex2 = (j+yy+1)*x + (i+xx+1);
+					int pixelIndex3 = (j+yy+1)*x + (i+xx);
+					
+					uint8_t rr0 = image[3*pixelIndex0];
+					uint8_t gg0 = image[3*pixelIndex0 + 1];
+					uint8_t bb0 = image[3*pixelIndex0 + 2];
+					uint8_t rr1 = image[3*pixelIndex1];
+					uint8_t gg1 = image[3*pixelIndex1 + 1];
+					uint8_t bb1 = image[3*pixelIndex1 + 2];
+					uint8_t rr2 = image[3*pixelIndex2];
+					uint8_t gg2 = image[3*pixelIndex2 + 1];
+					uint8_t bb2 = image[3*pixelIndex2 + 2];
+					uint8_t rr3 = image[3*pixelIndex3];
+					uint8_t gg3 = image[3*pixelIndex3 + 1];
+					uint8_t bb3 = image[3*pixelIndex3 + 2];
+					// These need to be divided by 3
+					// Will be done in final sum
+					uint16_t roberts1 = rr0 + gg0 + bb0 - rr2 + gg2 + bb2;
+					uint16_t roberts2 = rr1 + gg1 + bb1 - rr3 + gg3 + bb3;
+					
+					robertsValues[robertsValuesIndex++] = std::sqrt(double(roberts1*roberts1 + roberts2*roberts2))/3.0;
+				}
+			}
+
+			// Average of Roberts cross applied over this block
+			double mean = std::accumulate(robertsValues, robertsValues + (7*7), 0.0)/(7*7);
+			map[j/4 * mapWidth + i/4] = mean;
+		}
+	}
+
 	maps.push_back(map);
 
 	////////////////////////////////////////
@@ -238,4 +310,8 @@ doubleColor ciexyzToCielab(doubleColor in) {
 
 doubleColor rgbToCielab(color in) {
 	return ciexyzToCielab(rgbToCiexyz(linearizeRgb(colorToDoubleColor(in))));
+}
+
+double colorToGrayscale(doubleColor in) {
+	return in.r * 0.299 + in.g * 0.587 + in.b * 0.114;
 }
